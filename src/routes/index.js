e54ca1e762c0c18210a258a430453c815687a482
gitego-brian <gitegob7@gmail.com>
2021-02-24 11:14:03 +0200

    chore(change docs) - change docs from jsDoc to json"

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,14 +1,17 @@
 import { Router } from 'express';
+import swaggerUi from 'swagger-ui-express';
 import welcome from '../controllers/welcome';
 import { notFound } from '../controllers/error';
 import authRoutes from './auth';
 import todoRoutes from './todos';
 import dlLogs from '../controllers/logs';
+import docs from '../config/docs/swagger.json';
 
 const router = Router();
 
 router.get('/', welcome);
 router.get('/api/logs/:key', dlLogs);
+router.use('/api/docs', swaggerUi.serve, swaggerUi.setup(docs));
 router.use('/api/auth', authRoutes);
 router.use('/api/todos', todoRoutes);
 
